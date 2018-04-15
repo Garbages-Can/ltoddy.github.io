8ae91c502fd14490e739bc486b211944d735d4e8
ltoddy <taoliu14@acm.org>
2018-04-15 12:49:37 +0800

    add prompot

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import css from './Footer.css';
+import ReactTooltip from 'react-tooltip'
 
 const Footer = () => (
   <div className={css.container}>
-    <a href="https://github.com/ltoddy/resume" target="_blank" className={css.logo}>
-      <i className="fab fa-github"/>
-    </a>
+    <div>本页面由React框架构建</div>
+
+    <div className={css.logo}>
+      <a href="https://github.com/ltoddy/resume" target="_blank" data-tip data-for='src'>
+        <i className="fab fa-github"/>
+      </a>
+    </div>
+    <ReactTooltip id='src'>源代码</ReactTooltip>
+
+    <div>本页面由React框架构建</div>
   </div>
 );
 
