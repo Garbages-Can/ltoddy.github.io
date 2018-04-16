91b54d4bbbff37a4bba0f61fab6014e4326e7921
ltoddy <taoliu14@acm.org>
2018-04-16 09:26:08 +0800

    update

--- src/App.js
+++ src/App.js
@@ -16,6 +16,36 @@ class App extends React.Component {
           createdAt: "2018-04-14",
           link: "../essay/2018/04/14/deploy-sanic.html",
           previews: "最近云服务提供商在打价格战,我在滴滴云上花了很少的钱租了一个月的云服务器: 公网ip是： 116.85.42.182, 以下我以116.85.42.182这个ip为演示,当你自己在部署的时候请换成自己的ip地址."
+        },
+        {
+          title: "基于docker+gunicorn部署sanic项目",
+          createdAt: "2018-04-14",
+          link: "../essay/2018/04/14/deploy-sanic.html",
+          previews: "最近云服务提供商在打价格战,我在滴滴云上花了很少的钱租了一个月的云服务器: 公网ip是： 116.85.42.182, 以下我以116.85.42.182这个ip为演示,当你自己在部署的时候请换成自己的ip地址."
+        },
+        {
+          title: "基于docker+gunicorn部署sanic项目",
+          createdAt: "2018-04-14",
+          link: "../essay/2018/04/14/deploy-sanic.html",
+          previews: "最近云服务提供商在打价格战,我在滴滴云上花了很少的钱租了一个月的云服务器: 公网ip是： 116.85.42.182, 以下我以116.85.42.182这个ip为演示,当你自己在部署的时候请换成自己的ip地址."
+        },
+        {
+          title: "基于docker+gunicorn部署sanic项目",
+          createdAt: "2018-04-14",
+          link: "../essay/2018/04/14/deploy-sanic.html",
+          previews: "最近云服务提供商在打价格战,我在滴滴云上花了很少的钱租了一个月的云服务器: 公网ip是： 116.85.42.182, 以下我以116.85.42.182这个ip为演示,当你自己在部署的时候请换成自己的ip地址."
+        },
+        {
+          title: "基于docker+gunicorn部署sanic项目",
+          createdAt: "2018-04-14",
+          link: "../essay/2018/04/14/deploy-sanic.html",
+          previews: "最近云服务提供商在打价格战,我在滴滴云上花了很少的钱租了一个月的云服务器: 公网ip是： 116.85.42.182, 以下我以116.85.42.182这个ip为演示,当你自己在部署的时候请换成自己的ip地址."
+        },
+        {
+          title: "基于docker+gunicorn部署sanic项目",
+          createdAt: "2018-04-14",
+          link: "../essay/2018/04/14/deploy-sanic.html",
+          previews: "最近云服务提供商在打价格战,我在滴滴云上花了很少的钱租了一个月的云服务器: 公网ip是： 116.85.42.182, 以下我以116.85.42.182这个ip为演示,当你自己在部署的时候请换成自己的ip地址."
         }
       ]
     };
