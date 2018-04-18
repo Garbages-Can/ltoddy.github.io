8869dfb46035cab50bd45aded95be59c85493ca9
ltoddy <taoliu14@acm.org>
2018-04-19 07:47:30 +0800

    add icons

--- src/content/Essay.js
+++ src/content/Essay.js
@@ -6,7 +6,7 @@ import css from './Essay.css';
 const Essay = (props) => (
   <TimelineEvent title={props.title}
                  createdAt={props.createdAt}
-                 icon={<i className="material-icons md-18"/>}
+                 icon={<i className="fas fa-book" style={{ "fontSize": "16px" }} />}
   >
     <div className={css.preview}>
       <a href={props.link} target="_blank" data-tip data-for={props.title}>{props.preview}</a>
