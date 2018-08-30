52bd6aa6df5518ff22d1aef3bc33306a7d4818f0
ltoddy <taoliu14@acm.org>
2018-08-30 10:45:01 +0800

    webpack config

---
+++ react-webpack/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index.js'),
+
+  output: {
+    publicPath: 'dist/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
