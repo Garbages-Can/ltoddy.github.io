1c2b67fbaf8bad5ef610358c8c09f77e07b5693c
ltoddy <taoliu14@acm.org>
2018-09-08 09:09:17 +0800

    add 404 page

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import App from './App';
+import PageNotFound from './404';
 
 
 const createRouter = () => (
   <Router>
     <Switch>
       <Route exact path='/' component={App}/>
+      <Route component={PageNotFound}/>
     </Switch>
   </Router>
 );
