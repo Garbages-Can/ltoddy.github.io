9a6e1f541d61dafddfab251c6a5ad9b2a8b91a2e
LiuTao <taoliu14@acm.org>
2018-11-03 21:11:38 +0800

    Revert "Home"

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import HomeApp from './home/App';
-import BlogApp from './blog/App';
+import App from './blog/App';
 import PageNotFound from './404';
 
 
 const createRouter = () => (
   <Router>
     <Switch>
-      <Route exact path='/' component={HomeApp}/>
-      <Route exact path='/blog' component={BlogApp}/>
+      <Route exact path='/' component={App}/>
       <Route component={PageNotFound}/>
     </Switch>
   </Router>
